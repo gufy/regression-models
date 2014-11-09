77d3025a4e8b41061926358f4af7d00aec38ec1f
Vojtech Kopal <vojtech.kopal@gmail.com>
2014-11-09 23:07:21 +0100

    Implemented Weierstrass function #16

--- 2014-11-07/playground.m
+++ 2014-11-07/playground.m
@@ -47,8 +47,41 @@ for i = 1:N
     
 end
 
-xscale = -5:0.05:5 + x_opt_val(1);
-yscale = -5:0.05:5 + x_opt_val(2);
+xscale = -3:0.05:3 + x_opt_val(1);
+yscale = -3:0.05:3 + x_opt_val(2);
 [XS, YS] = meshgrid(xscale,yscale);
 ZS = griddata(X(1,:),X(2,:),Y, XS, YS);
 meshc(XS, YS, ZS);
+
+%%
+
+D = 2;
+N = 10000;
+
+range = 5;
+xmin = -range; xmax = range;
+ymin = -range; ymax = range;
+
+x_opt_val = zeros(D,1);%x_opt(D);
+f_opt_val = f_opt();
+[Q, R] = qr(randn(D));
+
+X = zeros(D, N);
+Y = zeros(N, 1);
+    
+for i = 1:N
+    
+    x = rand(D,1)*(xmax - xmin) + xmin;
+    X(:, i) = x;
+    
+    y = f_16(x, x_opt_val, f_opt_val, R, Q);
+    Y(i) = y;
+    
+end
+
+xscale = xmin:0.05:xmax + x_opt_val(1);
+yscale = ymin:0.05:ymax + x_opt_val(2);
+[XS, YS] = meshgrid(xscale,yscale);
+ZS = griddata(X(1,:),X(2,:),Y, XS, YS);
+meshc(XS, YS, ZS);
+axis([xmin xmax ymin ymax]);
--- libs/data_gen/f_16.m
+++ libs/data_gen/f_16.m
@@ -1,9 +1,37 @@
 function [ y ] = f_16( x, x_opt, f_opt, R, Q )
 % Weierstrass Function
+% One parameter version:
+% a = 1/2;
+% b = 3;
+% x = 0:0.0001:1;
+% y = zeros(1, length(x));
+% for i = 1:length(x)
+%     for k = 1:11
+%         y(i) = y(i) + a^k * cos(b^k * pi * x(i));
+%     end
+% end
+%
+% plot(x,y);
+
 
 D = length(x);
 z = R * lambda(1/100, D) * Q * T_osz(R*(x - x_opt));
-y = 10 * (D - sum(cos(2*pi*z))) + sum(z.^2) + f_opt;
+
+f_0 = 0;
+
+for k = 0:11
+    f_0 = f_0 + (1/2)^k * cos(pi*3^k);
+end
+
+y = 0;
+
+for i = 1:D
+    for k = 0:11
+        y = y + (1/2)^k * cos(2*pi*3^k*(z(i) + 1/2));
+    end
+end
+
+y = 10*(y/D - f_0)^3 + 10/D*f_pen(x) + f_opt;
 
 end
 
