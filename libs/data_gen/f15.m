a68d5c87d0946ef101ab3a7da1d92f65725004d1
Vojta Kopal <vojtech.kopal@gmail.com>
2014-11-28 13:10:46 +0100

    f15 amends

--- libs/data_gen/f15.m
+++ libs/data_gen/f15.m
@@ -4,7 +4,7 @@ function [ f ] = f15( D, x_opt, f_opt, R, Q )
         z = R * lambda(10, D) * Q * T_asy(T_osz(R*(x - x_opt)),0.2);
         %z = R * lambda(10, D) * Q * R*(x - x_opt);
         %z = R * lambda(10, D) * Q * R * (x - x_opt);
-        %z = T_asy(T_osz((x - x_opt)),0.2);
+        z = R * Q * T_asy(T_osz(R * (x - x_opt)),0.2);
         res = 10 * (D - sum(cos(2*pi*z))) + sum(z.^2) + f_opt;
     end
 
