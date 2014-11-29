97624c568b6213109056bd961bcd3097c5bf54f9
Vojta Kopal <vojtech.kopal@gmail.com>
2014-11-29 13:16:47 +0100

    playing with rbf nn

--- setup.m
+++ setup.m
@@ -2,6 +2,6 @@ me = mfilename;                                            % what is my filename
 mydir = which(me); mydir = mydir(1:end-2-numel(me));        % where am I located
 addpath(mydir(1:end-1))
 addpath([mydir,'libs'])
-addpath([mydir,'libs','/','data-gen'])
-addpath([mydir,'libs','/','data-gen','/','common'])
+addpath([mydir,'libs','/','data_gen'])
+addpath([mydir,'libs','/','data_gen','/','common'])
 addpath([mydir,'sims'])
