9c26a2d8f55b658398aad8eba074fd7f4362d8ed
Vojta Kopal <vojtech.kopal@gmail.com>
2015-01-17 11:45:13 +0100

    updated f21 2d 900

--- process/f21_cf_2d.m
+++ process/f21_cf_2d.m
@@ -25,7 +25,7 @@ I = 0;
 
 I = I + 1;
 models{I} = struct('name', 'SVM', 'model', @svmSim);
-models{1}.params = {{3}, {2}, { 0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20, 30, 50}};
+models{1}.params = {{3}, {2}, { 0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20, 30, 50, 100, 150, 200, 250, 300, 350, 400, 500, 600, 700}};
 
 I = I + 1;
 models{I} = struct('name', 'Poly', 'model', @polyfitSim);
@@ -33,15 +33,15 @@ models{I}.params = {{'quadratic'}};
 
 I = I + 1;
 models{I} = struct('name', 'RBF-NN', 'model', @nnSim);
-models{I}.params = {{4,2,1.5,1.2,1.1,1,0.1,0.01,0.001},{0.01, 0.001, 0.0001, 0.00001},{25, 50, 75, 100,200,300,400}};
+models{I}.params = {{4,2,1.5,1.2,1.1,1,0.1,0.01,0.001},{0.01, 0.001, 0.0001, 0.00001},{25, 50, 75,100,150,200,250,300,350,400,450,500}};
 
 I = I + 1;
 models{I} = struct('name', 'Forests', 'model', @forestsSim);
-models{I}.params = {num2cell(50:50:750), {1, 2, 5, 10, 20, 50, 100}};
+models{I}.params = {num2cell(50:50:900), {1, 2, 5, 10, 20, 50}};
 
 I = I + 1;
 models{I} = struct('name', 'GP', 'model', @gpSim);
-models{I}.params = {{1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 0.001, 0.01, 0.1, 0.2, 0.5, 1, 1.5, 2}, {0}};
+models{I}.params = {{1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 0.001, 0.01, 0.1, 0.2, 0.5, 1, 1.5, 2, 4, 8, 10}, {0}};
 
 results = crossValidateModelsWithParams(models, sX, sY, @(results, models) ...
     save(['data/',name,'.mat'], 'results', 'models') ...
