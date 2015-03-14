cb33ac196d87c28c918b028130063ddc3c4f58fe
Vojta Kopal <vojtech.kopal@gmail.com>
2015-03-14 15:14:36 +0100

    updated shell scripts

--- comp.sh
+++ comp.sh
@@ -1,5 +1,8 @@
 F=$1;
 D=$2;
 N=$3;
+Settings=$4;
 
-screen "cd ~/Documents/MATLAB/regression-models/; ulimit -t unlimited; nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r \"try, comp_f(15,5,5000,'settings_gp',1); end; exit\"" &
+cd ~/Documents/MATLAB/regression-models/
+ulimit -t unlimited
+nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r "try, comp_f("$F","$D","$N",'"$Settings"',1); end; exit" &
--- push.sh
+++ push.sh
@@ -2,9 +2,18 @@ PC=$1;
 F=$2;
 D=$3;
 N=$4;
+Settings=$5;
 
 echo "On u-pl"$PC" I'm running simulation for f"$F
 
-COMMAND="cd ~/Documents/MATLAB/regression-models; screen ./comp.sh "$F $D $N;
+COMMAND="cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N
 
-ssh "u-pl"$PC $COMMAND
+T=`date`
+LOG="logs/log-"$F"-"$D"-"$N"-"$Settings".txt";
+
+echo $T >>$LOG;
+
+echo $COMMAND
+ssh "u-pl"$PC "cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N" "$Settings >>$LOG 
+#screen -S "pc-"$PC ssh -t "u-pl"$PC "cd ~/Documents/MATLAB/regression-models/; ./comp.sh "$F" "$D" "$N" "$Settings 
+
