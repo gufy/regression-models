509f386536cb6ed5ac5e9e0da56242225f935f27
Vojta Kopal <vojtech.kopal@gmail.com>
2015-03-18 08:14:32 +0100

    update use get request to save data

--- libs/store.m
+++ libs/store.m
@@ -1,15 +1,12 @@
 function [ ] = store( method, dataset_name, datetime_started, models, results )
 
-    if nargin < 5
-        save2db = 0;
-    end
-
     results.method = method;
     results.dataset = dataset_name;
-    results.parameters = ['"', struct2str(models), '"'];
-    paramstr = struct2str(results, ' ');
+    results.parameters = struct2str(models);
     
-    system(['php php/save.php ', paramstr]);
+    paramstr = struct2str(results, '&');
+    %system(['php php/save.php ', paramstr]);
+    urlread(['http://vojtechkopal.cz/regressions/save.php?', paramstr]);
 
 end
 
