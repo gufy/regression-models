7fa7851fd505a8c4cd05d3d7372b347b3d73d260
Vojta Kopal <vojtech.kopal@gmail.com>
2015-06-28 12:26:30 -0400

    fix

--- push.sh
+++ push.sh
@@ -7,14 +7,14 @@ Noisy=$6
 
 echo "On u-pl"$PC" I'm running simulation for f"$F
 
-COMMAND="cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N
+COMMAND="cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N" "$Settings" "$Noisy
 
 T=`date`
-LOG="logs/log-"$F"-"$D"-"$N"-"$Settings".txt";
+LOG="logs/log-"$F"-"$D"-"$N"-"$Settings"-"$Noisy".txt";
 
 echo $T >>$LOG;
 
 echo $COMMAND
-ssh "u-pl"$PC "cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N" "$Settings" "$6 >>$LOG 
+ssh "u-pl"$PC "cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N" "$Settings" "$Noisy >>$LOG 
 #screen -S "pc-"$PC ssh -t "u-pl"$PC "cd ~/Documents/MATLAB/regression-models/; ./comp.sh "$F" "$D" "$N" "$Settings 
 
