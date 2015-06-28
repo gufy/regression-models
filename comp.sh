56957f44305ed51ee501e120d2efeb68ac4bb3fd
Vojta Kopal <vojtech.kopal@gmail.com>
2015-06-28 11:15:51 -0400

    amends from server

--- comp.sh
+++ comp.sh
@@ -4,6 +4,6 @@ N=$3;
 Settings=$4;
 Noisy=$5
 
-cd ~/Documents/MATLAB/regression-models/
+cd ~/Documents/MATLAB/regressions/
 ulimit -t unlimited
-nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r "try, comp_f("$F","$D","$N",'"$Settings"',1,"$Noisy"); end; exit" &
+nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r "try, comp_f("$F","$D","$N",'"$Settings"',0,"$Noisy"); end; exit" &
--- push.sh
+++ push.sh
@@ -3,11 +3,11 @@ F=$2;
 D=$3;
 N=$4;
 Settings=$5;
-Noisy=$6;
+Noisy=$6
 
 echo "On u-pl"$PC" I'm running simulation for f"$F
 
-COMMAND="cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N
+COMMAND="cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N
 
 T=`date`
 LOG="logs/log-"$F"-"$D"-"$N"-"$Settings".txt";
@@ -15,6 +15,6 @@ LOG="logs/log-"$F"-"$D"-"$N"-"$Settings".txt";
 echo $T >>$LOG;
 
 echo $COMMAND
-ssh "u-pl"$PC "cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N" "$Settings" "$Noisy >>$LOG 
+ssh "u-pl"$PC "cd ~/Documents/MATLAB/regressions; ./comp.sh "$F" "$D" "$N" "$Settings" "$6 >>$LOG 
 #screen -S "pc-"$PC ssh -t "u-pl"$PC "cd ~/Documents/MATLAB/regression-models/; ./comp.sh "$F" "$D" "$N" "$Settings 
 
