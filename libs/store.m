29ce28d63618a2f3728b6dfa247e7557d453d9a5
Vojta Kopal <vojtech.kopal@gmail.com>
2015-06-28 11:23:19 -0400

    noisy support

--- libs/store.m
+++ libs/store.m
@@ -1,9 +1,10 @@
-function [ ] = store( method, dataset_name, datetime_started, models, results, time )
+function [ ] = store( method, dataset_name, datetime_started, models, results, time, noisy )
 
     results.method = method;
     results.dataset = dataset_name;
     results.parameters = struct2str(models);
     results.time = time;
+    results.noisy = noisy;
     
     paramstr = struct2str(results, '&');
     %system(['php php/save.php ', paramstr]);
