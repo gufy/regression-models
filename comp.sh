16369387d86efc6364dff794cb7453292eef7072
Vojta Kopal <vojtech.kopal@gmail.com>
2015-06-28 11:13:27 -0400

    noisy support, shells

--- comp.sh
+++ comp.sh
@@ -2,7 +2,8 @@ F=$1;
 D=$2;
 N=$3;
 Settings=$4;
+Noisy=$5
 
 cd ~/Documents/MATLAB/regression-models/
 ulimit -t unlimited
-nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r "try, comp_f("$F","$D","$N",'"$Settings"',1); end; exit" &
+nice -n 19 /afs/ms/@sys/bin/matlab -nodisplay -r "try, comp_f("$F","$D","$N",'"$Settings"',1,"$Noisy"); end; exit" &
--- push.sh
+++ push.sh
@@ -3,6 +3,7 @@ F=$2;
 D=$3;
 N=$4;
 Settings=$5;
+Noisy=$6;
 
 echo "On u-pl"$PC" I'm running simulation for f"$F
 
@@ -14,6 +15,6 @@ LOG="logs/log-"$F"-"$D"-"$N"-"$Settings".txt";
 echo $T >>$LOG;
 
 echo $COMMAND
-ssh "u-pl"$PC "cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N" "$Settings >>$LOG 
+ssh "u-pl"$PC "cd ~/Documents/MATLAB/regression-models; ./comp.sh "$F" "$D" "$N" "$Settings" "$Noisy >>$LOG 
 #screen -S "pc-"$PC ssh -t "u-pl"$PC "cd ~/Documents/MATLAB/regression-models/; ./comp.sh "$F" "$D" "$N" "$Settings 
 
