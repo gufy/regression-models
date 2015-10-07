c735b798c02f4ee1c7a91d1a0587d6d9eb992057
Vojta Kopal <vojtech.kopal@gmail.com>
2015-10-07 10:25:58 +0200

    updated thesis content

--- thesis/code/gen_images_forests.m
+++ thesis/code/gen_images_forests.m
@@ -30,3 +30,37 @@ for I = 1:length(Nums)
     saveas(gcf, ['thesis/images/rforests', num2str(Num), '.pdf'], 'pdf')
 
 end
+
+%%
+
+
+Nums = [40 20 10 5 2 1];
+for I = 1:length(Nums)
+    
+    MinLeaf = Nums(I);
+    lin = -4:0.1:4;
+    sin_y = sin(lin);
+    
+    f = figure;
+    %f = subplot(2, 2, I);
+    plot(lin, sin_y);
+    hold on;
+
+    N = 60;
+    X = [rand(1,N/2)*3-4 rand(1,N/2)*3+1];
+    T = sin(X) + randn(1, N)*0.1;
+    scatter(X, T);
+    axis([-4 4 -2.5 2.5]);
+
+    %
+
+    model = forestsSim(X', T', struct('NTrees', 40, 'MinLeaf', MinLeaf, 'NVarToSample', 1));
+    
+    plotTrained1dModel(model.predict, 100, -4, 4, 0, f);
+    %title([num2str(Num) ' Trees']);
+    
+    set(gcf, 'PaperPosition', [-0.5 -0.25 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
+    set(gcf, 'PaperSize', [5 5]); %Keep the same paper size
+    saveas(gcf, ['thesis/images/rforests_l_', num2str(MinLeaf), '.pdf'], 'pdf')
+
+end
--- thesis/code/gen_times.m
+++ thesis/code/gen_times.m
@@ -5,7 +5,7 @@ xlabel('Dimensions');
 ylabel('Time [s]');
 clear time;
 
-set(gca,'FontSize', 28);
+set(gca,'FontSize', 16);
 set(gcf, 'PaperPosition', [0.25 0.1 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
 set(gcf, 'PaperSize', [6 5.75]); %Keep the same paper size
 saveas(gcf, ['thesis/images/forests_time.pdf'], 'pdf');
@@ -21,7 +21,7 @@ xlabel('Dimensions');
 ylabel('Time [s]');
 clear time;
 
-set(gca,'FontSize', 28);
+set(gca,'FontSize', 16);
 set(gcf, 'PaperPosition', [0.25 0.1 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
 set(gcf, 'PaperSize', [6 5.75]); %Keep the same paper size
 saveas(gcf, ['thesis/images/rbf_time.pdf'], 'pdf');
@@ -35,7 +35,7 @@ xlabel('Dimensions');
 ylabel('Time [s]');
 clear time;
 
-set(gca,'FontSize', 28);
+set(gca,'FontSize', 16);
 set(gcf, 'PaperPosition', [0.25 0.1 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
 set(gcf, 'PaperSize', [6 5.75]); %Keep the same paper size
 saveas(gcf, ['thesis/images/gp_time.pdf'], 'pdf');
@@ -49,7 +49,21 @@ ylabel('Time [s]');
 set(gca,'YScale','log');
 clear time;
 
-set(gca,'FontSize', 28);
+set(gca,'FontSize', 16);
 set(gcf, 'PaperPosition', [0.25 0.1 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
 set(gcf, 'PaperSize', [6 5.75]); %Keep the same paper size
 saveas(gcf, ['thesis/images/poly_time.pdf'], 'pdf');
+
+%%
+
+time = import_time('time_svm');
+boxplot(time.time, time.dim);
+xlabel('Dimensions');
+ylabel('Time [s]');
+set(gca,'YScale','log');
+clear time;
+
+set(gca,'FontSize', 16);
+set(gcf, 'PaperPosition', [0.25 0.1 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
+set(gcf, 'PaperSize', [6 5.75]); %Keep the same paper size
+saveas(gcf, ['thesis/images/svm_time.pdf'], 'pdf');
